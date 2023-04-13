39c1f30f508b9570de02a15c8f280ac1c177b9c6
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-13 18:06:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,12 +5,8 @@ from scipy.stats import norm
 chat_id = 238786813 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, x_cnt, y_success, y_cnt):
-    p_control = x_success / x_cnt
-    p_test = y_success / y_cnt
-
-    SE = np.sqrt((p_control * (1 - p_control) / x_cnt) + (p_test * (1 - p_test) / y_cnt))
-
-    z = (p_test - p_control) / SE
-    z_critical = norm.ppf(1 - 0.05 / 2)
-
-    return z > z_critical
+    p1, p2 = x_success/x_cnt, y_success/y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (p1 - p2)/np.sqrt(p*(1-p)*(1/x_cnt + 1/y_cnt))
+    
+    return z < -1.645
