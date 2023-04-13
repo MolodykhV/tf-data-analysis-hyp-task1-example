c2857f442d3d75e313f85478313636721e5f37f3
MolodykhV <129886700+MolodykhV@users.noreply.github.com>
2023-04-13 19:22:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,4 +9,4 @@ def solution(x_success: int, x_cnt, y_success, y_cnt):
     p = (x_success + y_success) / (x_cnt + y_cnt)
     z = (p1 - p2)/np.sqrt(p*(1-p)*(1/x_cnt + 1/y_cnt))
     
-    return z < -1.645
+    return z > -1.645
